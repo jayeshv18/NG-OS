522db3b848a47357836d4d2406c818983c2d81de
Jayeshv18 <jayeshv@karunya.edu.in>
2026-06-02 21:47:50 +0530

    feat(memory): activate MMU and identity map kernel
    Construct Two-Tier Page Directory and Page Table architecture. Allocate aligned physical memory for the master index. Implement an identity map for the first 4MB of RAM to prevent kernel page faults. Execute inline assembly to load CR3 and enable the MMU via CR0, successfully transitioning to Virtual Memory.

---
+++ src/include/paging.h
@@ -0,0 +1,25 @@
+#ifndef PAGING_H
+#define PAGING_H
+#include <stdint.h>
+
+/*
+* In C, when you write that line without the word extern or without putting it inside a struct, you are not just describing a blueprint. You are telling the compiler: "Physically allocate 4096 bytes of memory right here, right now, and name it page_table."
+Because this is a header file (.h), every single time a different .c file includes it (e.g., kernel.c, paging.c, memory.c), the compiler will create a brand new, separate 4096-byte array named page_table.
+When the Linker tries to glue your OS together, it will see five different arrays all named page_table, throw a Multiple Definition Error, and refuse to build the OS.
+ */
+
+typedef struct {
+ //this is exactly 4096 bytes long. It holds 1024 individual entries.
+ //each entry is a 32-bit integer containing:
+ // [ Top 20 Bits: Physical Block Address ] + [ Bottom 12 Bits: Security Flags ]
+ uint32_t entries[1024];
+} page_table; //an array of 1024 integers (Each integer holds a physical address + 12 flags).
+
+typedef struct {
+// this is also exactly 4096 bytes long. It holds 1024 individual entries.
+// each entry is a 32-bit integer containing:
+// [ Top 20 Bits: Physical Address of a Page Table ] + [ Bottom 12 Bits: Security Flags ]
+ uint32_t entries[1024];
+} page_directory; //array of 1024 integers (Each integer holds the physical address of a Page Table + 12 flags).
+
+#endif
---
+++ src/paging.c
@@ -0,0 +1,113 @@
+#include "include/paging.h"
+
+#include "include/pmm.h"
+#include "include/vga.h"
+// in this file, we need to create the global variable that will hold our one and only Kernel Page Directory.
+page_directory* kernel_directory=0;
+//the CPU's MMU strictly requires that every Page Directory and every Page Table must be mathematically perfectly aligned to a 4096-byte boundary in physical RAM.
+//if we just use standard C to say page_directory_t kernel_dir;, the GCC compiler will place it wherever it wants in memory (e.g., address 0x105004).
+//If the address doesn't end in 000, the MMU's bit-shifting math will completely shatter, and the CPU will Triple Fault instantly when we turn it on.
+
+/*
+ * remember that pmm_alloc_block() returns 0xFFFFFFFF if the physical RAM is completely exhausted.
+ * if we don't catch that, our OS will attempt to build its master memory map at address 0xFFFFFFFF, which will instantly crash the motherboard.
+ * because we are using pmm_alloc_block(), the PMM's internal math mathematically guarantees that the memory address it hands back is a perfect multiple of 4096.
+ *
+ * just like the PMM's "Default Deny" loop where we wrote 0xFFFFFFFF to lock everything, the Page Directory needs its own lockdown loop.
+ * if we don't clear it, the array will contain whatever random garbage data was left in that RAM chip when the computer powered on, and the CPU will try to execute it.
+ */
+
+void paging_init() {
+    uint32_t directory_address=pmm_alloc_block(); //requesting a perfectly 4KB-aligned physical block from our allocator
+    //security guard to check if we ran off ram...
+    if (directory_address==0xFFFFFFFF) {
+        vga_print("FATAL OS ERROR: Out of physical memory for Page Directory!\n");
+        for (;;) {
+            __asm__ volatile("hlt");//system halt to prevent crash
+        }
+    }
+    //casting the raw physical address into our struct blueprint
+    kernel_directory = (page_directory*)directory_address;
+
+    for (int i=0;i<1024;i++) {
+        // We set the entry to 0x02. Why?
+        // Bit 0 (Present) = 0 (This tells the CPU: "This map is empty")
+        // Bit 1 (Read/Write) = 1 (This tells the CPU: "When we do fill this, allow writing")
+        // Bit 2 (User/Supervisor) = 0 (Ring 0 Kernel Only)
+        kernel_directory->entries[i] = 0x00000002;
+    }
+    vga_print("Page Directory aligned & initialized!\n");
+
+    //identity mapping
+    /*
+     * if we flip the MMU switch without this, the CPU will try to read the next instruction at 0x100000, realize it has no map for it, and the system will instantly die.
+     * we need to create a map that tells the CPU: "Hey, Virtual Address 0x100000 is located exactly at Physical Address 0x100000."
+     * by mapping the first 4 Megabytes of memory exactly to itself, the kernel won't even notice when physical reality is replaced by virtual reality. It will just keep executing.
+     */
+
+    uint32_t page_table_address=pmm_alloc_block();
+    if (page_table_address==0xFFFFFFFF) {
+        vga_print("FATAL OS ERROR: Out of physical memory for Page Table!\n");
+        for (;;) {
+            __asm__ volatile("hlt");
+        }
+    }
+    page_table* first_page_table = (page_table*)page_table_address;
+
+    //fill the Page Table
+    for (int i=0;i<1024;i++) {
+        uint32_t phys_addr=i*4096; //calculate the physical address (0x0, 0x1000, 0x2000...)
+        // bitwise OR with 3 (Binary: 00000011)
+        // bit 0: Present (1)
+        // bit 1: Read/Write (1)
+        first_page_table->entries[i] = phys_addr | 3; //single Page Table has exactly 1024 entries (first_page_table->entries[i]).
+        //each entry covers one 4KB block. 1024 * 4096 = 4,194,304 bytes (Exactly 4 Megabytes).
+    }
+    //link the Page Table into the Page Directory. We put it in slot [0] because it covers the bottom 4MB of RAM.
+    kernel_directory->entries[0]=page_table_address | 3;
+
+    //we need ti turn on the MMU, which is controlled by CR3 register and it can be turned on using asm.
+    vga_print("Flipping MMU Switch. Entering the Matrix...\n");
+    //load CR3 and flip CR0
+    __asm__ volatile (
+        //move the physical address of our Directory into EAX
+        "mov %0, %%eax\n"
+        //load EAX into CR3 (The CPU now knows where the map is)
+        "mov %%eax, %%cr3\n"
+        // read the current state of CR0 into EAX
+        "mov %%cr0, %%eax\n"
+        // bitwise OR EAX with 0x80000000 (This sets the 31st bit to 1)
+        "or $0x80000000, %%eax\n"
+        // write the modified EAX back into CR0 (PAGING IS NOW ON)
+        "mov %%eax, %%cr0\n"
+        :
+        : "r" (directory_address) //pass our directory address to %0
+        : "eax"
+    );
+
+    vga_print("Paging Active. We are running in Virtual Memory.\n");
+}
+
+
+
+
+/* basic paging explanation.
+imagine an apartment building (Physical RAM). It has physical addresses: Apartment 100, Apartment 101, Apartment 102.
+
+if we write a program and hardcode it to run at "Apartment 100," it will work perfectly.
+but what if we run two copies of that program? They both try to move into Apartment 100 at the same time. They fight, destroy each other's furniture (data), and the OS crashes.
+
+Paging is the OS creating an illusion.
+when we turn on Paging, the OS stops handing out real apartment numbers. Instead, it hands out Virtual Addresses.
+the OS tells Program A: "You live in Apartment 100." (But secretly, the OS puts Program A in Physical Apartment 205).
+the OS tells Program B: "You live in Apartment 100." (But secretly, the OS puts Program B in Physical Apartment 803).
+both programs think they own "Apartment 100." They never fight, because they are mathematically separated. Paging is the mechanism that creates secure sandboxes for every program running on our computer.
+
+Part 2: The Two-Tier Map (The Post Office)
+to keep track of this massive illusion, the CPU needs a translation map. If Program A asks for "Virtual Apartment 100," the CPU needs to quickly look up where that is physically located.
+because 4 Gigabytes of memory is massive, we can't use one giant list. We use a Two-Tier System, exactly like a Post Office routing a package.
+the Page Directory (The State Level): This is a small list (1024 entries). It doesn't tell you the exact apartment; it just tells you which city the package goes to.
+the Page Table (The City Level): This is another small list (1024 entries). Once you are in the right city, this list tells you the exact physical apartment number.
+every single process on your computer (and your kernel itself) gets exactly one Page Directory (The Master Index).
+ */
+
